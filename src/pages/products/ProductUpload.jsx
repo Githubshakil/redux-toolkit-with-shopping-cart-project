fbd37d541a290f0b1f146fec23c8d7798865421e
Githubshakil <shakilmahmud007@gmail.com>
2025-08-09 21:53:11 +0600

    update

--- src/pages/products/ProductUpload.jsx
+++ src/pages/products/ProductUpload.jsx
@@ -1,21 +1,44 @@
-import React from 'react'
+import React from "react";
 
 const ProductUpload = () => {
   return (
-    <div className='max-w-md mx-auto bg-white shadow-md rounded-lg p-6'>
-      <h2 className='text-xl font-semibold mb-4 text-gray-700'>Add New Product</h2>
+    <div className="max-w-md mx-auto bg-white shadow-md rounded-lg p-6">
+      <h2 className="text-xl font-semibold mb-4 text-gray-700">
+        Add New Product
+      </h2>
 
-
-
-      <form>
+      <form className="space-y-4">
         {/* name */}
         <div>
-          <label className='text-sm font-medium block text-gray-600'>Product Name</label>
-          <input type="text" name='name' id='name' placeholder='Add product name' className='w-full bg-gray-50 text-gray-700 mt-1 p-2 rounded-lg focus:outline focus:ring-1 border border-gray-200' />
+          <label className="text-sm font-medium block text-gray-600">
+            Product Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Add product name"
+            className="w-full bg-gray-50 text-gray-700 mt-1 p-2 rounded-lg focus:outline focus:ring-1 border border-gray-200 focus:ring-indigo-500"
+          />
+        </div>
+
+        {/* category */}
+
+        <div>
+          <label className="text-sm font-medium block text-gray-600">
+            Category
+          </label>
+          <select id="category" name="category" className="w-full bg-gray-50 text-gray-700 mt-1 p-2 rounded-lg focus:outline focus:ring-1 border border-gray-200 focus:ring-indigo-500">
+            <option value="">Choose a Category</option>
+            <option value="fashion">Fashion</option>
+            <option value="gadgets">Gadgets</option>
+            <option value="bags">Bags</option>
+            <option value="electronics">Electronics</option>
+          </select>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default ProductUpload
+export default ProductUpload;
