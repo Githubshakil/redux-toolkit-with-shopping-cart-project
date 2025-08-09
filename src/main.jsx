b6a8767de2aca2fd3462f1f2ad3ace7cc0b2ccbc
Githubshakil <shakilmahmud007@gmail.com>
2025-08-09 20:02:05 +0600

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,24 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+import { BrowserRouter, Route, Routes } from "react-router";
+import HomePage from "./pages/home/HomePage.jsx";
+import ProductUpload from "./components/ProductUpload.jsx";
+import Product from "./pages/product/product.jsx";
 
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Provider } from 'react-redux'
-import store from './redux/store.js'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import HomePage from './pages/home/HomePage.jsx'
-import ProductUpload from './components/ProductUpload.jsx'
-import Product from './pages/product/product.jsx'
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
-    <Routes>
-      <Route element={<App/>}>
-      <Route path='/' element={<HomePage/>}/>
-      <Route path='/products' element={<Product/>}/>
-      <Route path='/cart' element={<div>Cart Page</div>}/>
-      <Route path='/items-upload' element={<ProductUpload/>}/>
-
-      </Route>
-    </Routes>
-  </BrowserRouter>,
-  </Provider>,
- 
-)
+      <Routes>
+        <Route element={<App />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<Product />} />
+          <Route path="/cart" element={<div>Cart Page</div>} />
+          <Route path="/items-upload" element={<ProductUpload />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+);
